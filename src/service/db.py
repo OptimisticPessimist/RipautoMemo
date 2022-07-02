835d045d4cc6cf94ad8f319b8f6a1d2e8ae28b34
OptimisticPessimist <wordsword.mk@gmail.com>
2022-07-02 12:34:23 +0900

    Add auto register with take from friend

--- src/service/db.py
+++ src/service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import JSON, Column, Date, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-from src.config import Config
+from src.config import TestConfig as Config
 from src.domain import User
 
 DB = Config.db
